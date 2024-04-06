bf3d8c3debe52d7fe1995a72596eac0f59207729
HARIRADHI <109915231+HARIRADHI@users.noreply.github.com>
2024-04-06 14:29:24 +0530

    Add files via upload

---
+++ likely.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+
+def main():
+    st.header("Diagnosis")
+    st.write("""Treatment for schizophrenia
+Although schizophrenia is a lifelong condition, it is treatable. Receiving timely and effective treatment can help manage symptoms and prevent relapses.
+
+Treatment options include:
+
+Antipsychotic drugs: Antipsychotic medications are available as daily oral doses or monthly injections. People who take antipsychotics as prescribed may experience less intense and less frequent psychotic symptoms. While effective, antipsychotic drugs can have adverse effects, such as weight gain and drowsiness.""")
+    st.header("Psychotherapy")
+    st.header("Cognitive behavioral therapy (CBT)")
+    st.write("""CBT can help people develop useful coping skills and strategies for working through disruptive thoughts.
+Psychodynamic therapy: Also known as psychoanalytic therapy, psychodynamic therapy involves conversations between a psychologist and their patient. These conversations attempt to uncover emotional experiences and unconscious processes that contribute to a person’s current mental state.""")
+    st.header("Acceptance and commitment therapy (ACT)")
+    st.write("""ACT is a type of behavioral therapy that encouragesTrusted Source people to accept, rather than challenge, their deep feelings. ACT also focuses on commitments to personal goals and values and improving one’s overall quality of life. Finally, ACT teaches mindfulness skills that can help keep a person focused on the present moment instead of being consumed by negative thoughts or experiences. Combining these three conditions, a person can change their behaviors by first changing their attitude toward themself.
+Family therapy: This form of psychotherapy involves families and significant others of people with schizophrenia and other mental health conditions. It focuses on education, stress reduction, and emotional processing. It helps family members better communicate and resolve conflicts with one another.
+Coordinated specialty care (CSC): CSC involves a team of health professionals who manage medication, deliver psychotherapy, and provide education and employment support.""")
+    st.header("Where to seek emergency care")
+    st.write("""
+If an individual is experiencing suicidal thoughts or exhibiting dangerous behaviors, please seek emergency care:
+
+Dial 911 or visit the nearest emergency department.
+Call the NAMI HelpLine at 1-800-950-NAMI (6264).
+Call the SAMHSA’s National Helpline at 1-800-622-HELP (4357).
+Find a local extended observation unit (EOU) or crisis stabilization unit (CSU).
+Suicide prevention
+If you know someone at immediate risk of self-harm, suicide, or hurting another person:
+
+Ask the tough question: “Are you considering suicide?”
+Listen to the person without judgment.
+Call 911 or the local emergency number, or text TALK to 741741 to communicate with a trained crisis counselor.
+Stay with the person until professional help arrives.
+Try to remove any weapons, medications, or other potentially harmful objects.
+If you or someone you know is having thoughts of suicide, a prevention hotline can help. The 988 Suicide and Crisis Lifeline is available 24 hours a day at 988. During a crisis, people who are hard of hearing can use their preferred relay service or dial 711 then 988.""")
+main()
